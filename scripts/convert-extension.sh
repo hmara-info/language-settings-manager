24569c3ecb9e2182bac4819eee2eb29716e593a7
Dmytro Lvivsky <dmytro.lvivsky@gmail.com>
2022-06-08 15:43:29 -0400

    chore: get rid of the convertion step in the scripts -- move it to the shell scripts

---
+++ scripts/convert-extension.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+xcrun safari-web-extension-converter --force --no-prompt --no-open --macos-only --bundle-identifier info.hmara.language-settings-manager --app-name Language-settings-manager --project-location safari build
