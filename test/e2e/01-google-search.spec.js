c40f2b6c3e97231cd2c8118cb325bb7ebb2bd74d
Yaroslav Korshak <ykorshak@gmail.com>
2024-09-12 19:50:13 +0200

    more tests for google search rewrite

--- test/e2e/01-google-search.spec.js
+++ test/e2e/01-google-search.spec.js
@@ -1,48 +1,47 @@
 require('./lu-puppeteer.js');
 
-describe('Google Search Results', () => {
-  beforeAll(async () => {
-    try {
-      await worker.evaluate(async () => {
-        await chrome.storage.sync.set({
-          userSettings: {
-            lessLanguages: ['ru'],
-            moreLanguages: ['uk'],
-          },
-        });
-      });
-    } catch (e) {
-      console.error('Error setting storage in worker:', e);
-    }
-  });
-
-  it('google-rewrite adds correct lr parameter to the url', async () => {
-    await page.setExtraHTTPHeaders({
-      'Accept-Language': 'ru-RU,ru',
-    });
-
-    await page.goto('https://www.google.com/search?q=test', {
-      waitUntil: 'networkidle0',
-    });
-    var url = await page.evaluate(() => document.location.href);
-    var lr = new URL(url).searchParams.get('lr');
-    expect(lr).toBe('(-lang_ru)');
-
-    await worker.evaluate(async () => {
+describe('Google Search Results handler', () => {
+  const testSetup = async (opts) => {
+    const lessLanguages = opts.lessLanguages;
+    await worker.evaluate(async (lessLanguages) => {
       await chrome.storage.sync.set({
         userSettings: {
-          collectStats: true,
-          is_18: true,
-          lessLanguages: ['en'],
+          lessLanguages: [...lessLanguages],
           moreLanguages: ['uk'],
         },
       });
+    }, lessLanguages);
+    await page.setExtraHTTPHeaders({
+      'Accept-Language': opts.acceptLanguage, // ,
     });
+
     await page.goto('https://www.google.com/search?q=test', {
       waitUntil: 'networkidle0',
     });
     var url = await page.evaluate(() => document.location.href);
     var lr = new URL(url).searchParams.get('lr');
-    expect(lr).toBe('(-lang_en)');
+    expect(lr).toBe(opts.expectedLr);
+  };
+
+  it('google-rewrite adds no lr parameter when lessLanguages is empty', async () => {
+    await testSetup({
+      lessLanguages: [],
+      acceptLanguage: 'uk-UA,uk',
+      expectedLr: '',
+    });
+  });
+
+  it('google-rewrite adds correct lr parameter to the url', async () => {
+    await testSetup({
+      lessLanguages: ['ru'],
+      acceptLanguage: 'uk-UA,uk',
+      expectedLr: '(-lang_ru)',
+    });
+
+    await testSetup({
+      lessLanguages: ['en'],
+      acceptLanguage: 'uk-UA,uk',
+      expectedLr: '(-lang_en)',
+    });
   });
 });
