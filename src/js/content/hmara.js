3613d878f73921380ebbf3f5a0d11f9c32833f28
Yaroslav Korshak <ykorshak@gmail.com>
2024-09-30 21:41:27 +0200

    adds support for hmara.info website

---
+++ src/js/content/hmara.js
@@ -0,0 +1,43 @@
+import defaultHandler from './default';
+import { storageGetSync, getExtensionVersion } from '../util';
+import browser from 'webextension-polyfill';
+
+export default class hmaraHandler extends defaultHandler {
+  handlerName = 'hmara';
+  /* Inherits the interface, but completely overrites base class behavior
+   */
+
+  async needToTweakLanguages() {
+    const $self = this;
+    /* Tweak main page content with user's progress */
+
+    // The extension is intalled, if this code runs. Thus checking
+    storageGetSync(null).then((data) => {
+      const moreLanguages = data.userSettings?.moreLanguages;
+      const lessLanguages = data.userSettings?.lessLanguages;
+      const achievements = Object.keys(data).filter((key) =>
+        key.startsWith('AC_')
+      );
+      const userId = data.userId;
+      const eventData = {
+        version: getExtensionVersion(),
+        isInstalled: true,
+        isConfigured: !!(moreLanguages && lessLanguages),
+        isExperienced: !!achievements?.length,
+        optionsUrl: browser.runtime.getURL('options.html'),
+        userId: userId,
+      };
+
+      const event = new CustomEvent('LU', {
+        detail: JSON.stringify(eventData),
+      });
+      $self.document.dispatchEvent(event);
+    });
+
+    return $self.NOOP;
+  }
+
+  async changeLanguageTo(languages) {
+    return $self.NOOP;
+  }
+}
--- src/js/routing.js
+++ src/js/routing.js
@@ -5,6 +5,7 @@ import wikipediaHandler from './content/wikipedia';
 import linkedinHandler from './content/linkedin';
 import youtubeHandler from './content/youtube';
 import googleMyaccountHandler from './content/google-myaccount';
+import hmaraHandler from './content/hmara';
 
 export function dispatch(location, document, moreLanguages, lessLanguages) {
   if (location.hostname.match(/\.facebook.com$/i)) {
@@ -23,6 +24,8 @@ export function dispatch(location, document, moreLanguages, lessLanguages) {
     return new linkedinHandler(...arguments);
   } else if (location.hostname.match(/www\.youtube\.com$/i)) {
     return new youtubeHandler(...arguments);
+  } else if (location.hostname.match(/(^|\.)hmara\.info$/i)) {
+    return new hmaraHandler(...arguments);
   } else {
     //return new defaultHandler(...arguments);
   }
