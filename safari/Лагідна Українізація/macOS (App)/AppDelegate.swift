e45684dfe342b09733af9b68cfd1bb798fc5d07b
Yaroslav Korshak <ykorshak@MacBookPro.home>
2025-07-08 21:27:17 +0200

    adds iOS support to safari wrapper

---
+++ safari/Лагідна Українізація/macOS (App)/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  macOS (App)
+//
+//  Created by Yaroslav Korshak on 08/07/2025.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
