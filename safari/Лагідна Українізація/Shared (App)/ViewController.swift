91a74dfe9e776f0c6f1344005efa8be35cb2f5c0
Yaroslav Korshak <yaroslav@resurgo.ai>
2025-10-11 11:36:16 +0200

    replaces webview with safari opening hosted onboarding page

--- safari/Лагідна Українізація/Shared (App)/ViewController.swift
+++ safari/Лагідна Українізація/Shared (App)/ViewController.swift
@@ -5,83 +5,33 @@
 //  Created by Yaroslav Korshak on 08/07/2025.
 //
 
-import WebKit
-
 #if os(iOS)
 import UIKit
+import WebKit
 typealias PlatformViewController = UIViewController
 #elseif os(macOS)
 import Cocoa
-import SafariServices
 typealias PlatformViewController = NSViewController
 #endif
 
 let extensionBundleIdentifier = "info.hmara.---------------------.Extension"
 
-class ViewController: PlatformViewController, WKNavigationDelegate, WKScriptMessageHandler {
+class ViewController: PlatformViewController {
 
     @IBOutlet var webView: WKWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.webView.navigationDelegate = self
-
-#if os(iOS)
-        self.webView.scrollView.isScrollEnabled = false
-#endif
-
-        self.webView.configuration.userContentController.add(self, name: "controller")
-
-        self.webView.loadFileURL(Bundle.main.url(forResource: "Main", withExtension: "html")!, allowingReadAccessTo: Bundle.main.resourceURL!)
-    }
-
-    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-#if os(iOS)
-        webView.evaluateJavaScript("show('ios')")
-#elseif os(macOS)
-        webView.evaluateJavaScript("show('mac')")
-
-        SFSafariExtensionManager.getStateOfSafariExtension(withIdentifier: extensionBundleIdentifier) { (state, error) in
-            guard let state = state, error == nil else {
-                // Insert code to inform the user that something went wrong.
-                return
-            }
-
-            DispatchQueue.main.async {
-                if #available(macOS 13, *) {
-                    webView.evaluateJavaScript("show('mac', \(state.isEnabled), true)")
-                } else {
-                    webView.evaluateJavaScript("show('mac', \(state.isEnabled), false)")
-                }
-            }
+        #if os(macOS)
+        if let url = Bundle.main.url(forResource: "Main", withExtension: "html") {
+            self.webView.loadFileURL(url, allowingReadAccessTo: url.deletingLastPathComponent())
         }
-#endif
-    }
-
-    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
-#if os(macOS)
-        if (message.body as! String != "open-preferences") {
-            return
+        #elseif os(iOS)
+        if let url = URL(string: "https://hmara.info/ios-setup") {
+            self.webView.load(URLRequest(url: url))
         }
-
-        SFSafariApplication.showPreferencesForExtension(withIdentifier: extensionBundleIdentifier) { error in
-            guard error == nil else {
-                // Insert code to inform the user that something went wrong.
-                return
-            }
-
-            DispatchQueue.main.async {
-                NSApp.terminate(self)
-            }
-        }
-#elseif os(iOS)
-        if let messageBody = message.body as? String, messageBody == "openSafari" {
-            if let url = URL(string: "https://hmara.info/ios-setup") {
-                UIApplication.shared.open(url)
-            }
-        }
-#endif
+        #endif
     }
 
 }
