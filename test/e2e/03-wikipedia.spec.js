19e03f4124b78c38446f0294fe2e8d25575196d2
Yaroslav Korshak <ykorshak@gmail.com>
2026-01-05 23:13:15 +0100

    fixes wikipedia brokem behaviour

--- test/e2e/03-wikipedia.spec.js
+++ test/e2e/03-wikipedia.spec.js
@@ -1,50 +1,61 @@
 require('./lu-puppeteer.js');
 
 describe('Wikipedia handler', () => {
-  beforeAll(async () => {
+  const setSpeed = async (speed) => {
     try {
-      await worker.evaluate(async () => {
+      await worker.evaluate(async (speedValue) => {
         await chrome.storage.sync.set({
           userSettings: {
             moreLanguages: ['uk'],
+            lessLanguages: ['ru'],
+            speed: speedValue,
           },
         });
-      });
+      }, speed);
     } catch (e) {
       console.error('Error setting storage in worker:', e);
     }
-  });
+  };
 
-  it('does not prompt the user when the page is in a desired language', async () => {
-    await page.goto('https://uk.wikipedia.org/wiki/Головна_сторінка', {
-      waitUntil: 'networkidle0',
-    });
-
-    const shadowHostPresent = await page.evaluate(async () => {
+  const checkShadowHostPresence = async () => {
+    return await page.evaluate(() => {
       const shadowHost = document.querySelector('#lu-shadow-host');
-      if (!shadowHost) {
-        return false;
-      }
+      return shadowHost !== null;
+    });
+  };
+
+  const testCases = [
+    {
+      description: 'does not prompt the user when the page is in a desired language',
+      url: 'https://uk.wikipedia.org/wiki/Головна_сторінка',
+      expectPrompt: false,
+    },
+    {
+      description: 'from undesired language page, prompts the user to go to a page in a desired language',
+      url: 'https://ru.wikipedia.org/wiki/Тест',
+      expectPrompt: true,
+    },
+    {
+      description: 'from neutral language page',
+      url: 'https://en.wikipedia.org/wiki/Main_Page',
+      expectPrompt: (speed) => speed === 'fast',
+    },
+  ];
+
+  describe.each(['fast', 'gentle'])('with %s speed', (speed) => {
+    beforeAll(async () => {
+      await setSpeed(speed);
     });
 
-    expect(shadowHostPresent).toBe(false);
-  });
-
-  it('prompts the user to go to a page in a desired language', async () => {
-    await page.goto('https://en.wikipedia.org/wiki/Main_Page', {
-      waitUntil: 'networkidle0',
+    testCases.forEach((testCase) => {
+      it(testCase.description, async () => {
+        await page.goto(testCase.url, { waitUntil: 'networkidle0' });
+        const shadowHostPresent = await checkShadowHostPresence();
+        const expectedPrompt = typeof testCase.expectPrompt === 'function'
+          ? testCase.expectPrompt(speed)
+          : testCase.expectPrompt;
+        expect(shadowHostPresent).toBe(expectedPrompt);
+      });
     });
-
-    const shadowHostPresent = await page.evaluate(async () => {
-      const shadowHost = document.querySelector('#lu-shadow-host');
-      if (!shadowHost) {
-        return false;
-      }
-      return true;
-      const promptDiv = shadowHost.querySelector('lahidnaUkrainizatsiya');
-      return promptDiv ? true : false;
-    });
-
-    expect(shadowHostPresent).toBe(true);
   });
 });
